46697110a7679cda8e71a888985a7dd15ff0e2ce
Felipe Perius <felipe.perius@gmail.com>
2017-02-27 16:27:56 -0300

    Request parse user

--- Boomerang/Boomerang/Persistence/UserRequest.swift
+++ Boomerang/Boomerang/Persistence/UserRequest.swift
@@ -7,7 +7,47 @@
 //
 
 import UIKit
+import Parse
+
 
 class UserRequest: NSObject {
+    static func createAccountUser (user: User, pass: String, completionHandler: @escaping (_ success: Bool, _ msg: String) -> Void) {
+        
+        let pfUser = PFUser()
+        
+        pfUser.username = user.name
+        pfUser.password = pass
+        pfUser.email = user.email
+        pfUser["emailVerified"] = false
+        
+        
+        pfUser.signUpInBackground { (success, error) in
+            
+            if error == nil {
+                completionHandler(true, "Sucesso")
+            } else {
+                
+                completionHandler(false, error.debugDescription)
+            }
+        }
+    }
     
+    
+    static func loginUserWithFacebook (id: String, email: String,userName: String ,mediaType:Int,completionHandler: @escaping (_ sucess: Bool, _ msg: String, _ user: User?) -> Void) {
+        
+        
+    }
+    
+    static func loginUser (email: String, pass: String, completionHandler: @escaping (_ success: Bool, _ msg: String) -> Void) {
+        
+        PFUser.logInWithUsername(inBackground: email, password: pass) { (success, error) in
+            
+            if error == nil {
+                completionHandler(true, "Sucesso")
+            } else {
+                completionHandler(false, error.debugDescription)
+            }
+        }
+    }
+
 }
--- Boomerang/Boomerang/ViewControllers/Authentication/Controllers/AuthenticationMainViewController.swift
+++ Boomerang/Boomerang/ViewControllers/Authentication/Controllers/AuthenticationMainViewController.swift
@@ -11,8 +11,7 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import FalconFrameworkIOSSDK
 import FacebookLogin
-import  FacebookCore
-import FacebookShare
+
 
 
 class AuthenticationMainViewController: UIViewController {
@@ -82,7 +81,7 @@ extension AuthenticationMainViewController {
                     let email = data["email"] as! String
                     let name = "\(data["first_name"] as! String) \(data["last_name"])"
                     
-                    self.getListFriends()
+                    
                     
                     FFAuthRequests.loginUserWithSocialMedia(socialMediaId: socialMediaId, email: email, name: name, socialMediaType: .facebook, socialMediaPasswordServerSecret: "AQWgd$j[QGe]Bh.Ugkf>?B3y696?2$#B2xwfN3hrVhFrE348g", autoStoreAuthTokenData: true) { (error, tokenReturnData) in
                         
@@ -100,31 +99,7 @@ extension AuthenticationMainViewController {
         }
     }
     
-    func getListFriends(){
-           
-//        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me/friends", parameters: nil)
-//        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
-//            
-//            if ((error) != nil)
-//            {
-//                // Process error
-//                print("Error: \(error)")
-//            }
-//            else
-//            {
-//                print("fetched user: \(result)")
-//                let userName : NSString = result.valueForKey("name") as! NSString
-//                print("User Name is: \(userName)")
-//                let userID : NSString = result.valueForKey("id") as! NSString
-//                print("User Email is: \(userID)")
-//                
-//                
-//                
-//            }
-//        })
-    
-    }
-    
+
 
    
 }
