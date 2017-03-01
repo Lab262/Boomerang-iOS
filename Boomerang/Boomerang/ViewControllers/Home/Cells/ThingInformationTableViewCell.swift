a010a740b7999368e8d6f06cd27084c5468417ca
Huallyd <huallyd@gmail.com>
2017-03-01 02:42:20 -0300

    outlets cell thing information

--- Boomerang/Boomerang/ViewControllers/Home/Cells/ThingInformationTableViewCell.swift
+++ Boomerang/Boomerang/ViewControllers/Home/Cells/ThingInformationTableViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ThingInformationTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet var evaluationButtons: [UIButton]!
+    @IBOutlet weak var thingImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var boomerUserLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionThingLabel: UILabel!
+    
+    static let identifier = "thingInformationCell"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        
     }
     
 }
