458ab80eae3646dda2b4e31991d94ed6d38aa4bd
Huallyd <huallyd@gmail.com>
2017-03-28 17:27:17 -0300

    Refactor of posts, generic method for saving PFObject, query for get all comments with author (Missing paging test), Detail Thing in pattern MVP

---
+++ Boomerang/Boomerang/Persistence/CommentRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CommentRequest.swift
+//  Boomerang
+//
+//  Created by Huallyd Smadi on 28/03/17.
+//  Copyright © 2017 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class CommentRequest: NSObject {
+
+}
