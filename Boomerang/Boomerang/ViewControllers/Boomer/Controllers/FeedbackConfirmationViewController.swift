20a94c90fd112cd3d881fa8c7e1923a5a739e092
Felipe Perius <felipe.perius@gmail.com>
2017-04-26 22:06:32 -0300

    adjust final flux create post

---
+++ Boomerang/Boomerang/ViewControllers/Boomer/Controllers/FeedbackConfirmationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedbackConfirmationViewController.swift
+//  Boomerang
+//
+//  Created by Felipe perius on 26/04/17.
+//  Copyright © 2017 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackConfirmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
