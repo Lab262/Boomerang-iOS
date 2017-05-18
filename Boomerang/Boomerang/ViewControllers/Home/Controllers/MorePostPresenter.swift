99b5c0aa300536922e8a4576e6a30bd1c9118c0e
Huallyd <huallyd@gmail.com>
2017-05-18 15:04:15 -0300

    More Posts View Controller and Presenter.

---
+++ Boomerang/Boomerang/ViewControllers/Home/Controllers/MorePostPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MorePostPresenter.swift
+//  Boomerang
+//
+//  Created by Huallyd Smadi on 18/05/17.
+//  Copyright © 2017 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class MorePostPresenter: NSObject {
+
+}
