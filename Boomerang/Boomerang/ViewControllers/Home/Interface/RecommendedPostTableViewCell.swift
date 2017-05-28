08d1a07dfb08d898209e5d41094ff266418c91ce
Huallyd <huallyd@gmail.com>
2017-05-28 02:28:06 -0300

    remove placeholder photo

--- Boomerang/Boomerang/ViewControllers/Home/Interface/RecommendedPostTableViewCell.swift
+++ Boomerang/Boomerang/ViewControllers/Home/Interface/RecommendedPostTableViewCell.swift
@@ -51,7 +51,7 @@ class RecommendedPostTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        loadPlaceholderImage(#imageLiteral(resourceName: "placeholder_image"), CGRect(x: self.frame.origin.x+10, y: frame.origin.y, width: 345, height: 288))
+       
         registerNib()
         initializePageIndicatorView()
     }
@@ -71,8 +71,8 @@ class RecommendedPostTableViewCell: UITableViewCell {
             
         cell.presenter.post = presenter.posts[indexPath.row]
         cell.setupCell()
-        unloadPlaceholderImage()
-            
+        
+        
         return cell
     }
     
