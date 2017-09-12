7e7ae2604fb60ad9398e8625f8b84a0a3a8a92bf
felipe <felipe.perius@gmail.com>
2017-09-11 21:30:08 -0300

    Implementantion collectionView Delegates and include page indicador custam

--- Boomerang/Boomerang/ViewControllers/Authentication/Controllers/AuthenticationMainViewController.swift
+++ Boomerang/Boomerang/ViewControllers/Authentication/Controllers/AuthenticationMainViewController.swift
@@ -14,22 +14,62 @@ import ParseFacebookUtilsV4
 
 class AuthenticationMainViewController: UIViewController {
     
-    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var facebookButton: UIButton!
     var presenter = AuthenticationPresenter()
-    
+    var pageIndicatorView: PageIndicatorView?
+    let spaceCells: CGFloat = 2
+    let sizeCells: Int = 8
+    @IBOutlet weak var viewPages: UIView!
     let defaultTextTitleWelcome = "Bem vindo"
     let defaultTextDescriptionWelcome = " a rede social mais amorzinho que você respeita"
     let defaultSizeFontWelcomeLabel:CGFloat = 14
+    let allText = ["Bem vindo a rede social mais amorzinho que você respeita"," Aqui todo mundo se conhece! Nossa rede é feita de amigos para amigos.","Entre com o Facebook para conseguir o código boomer"]
+    let allImages = [#imageLiteral(resourceName: "ic_heart_auth"),#imageLiteral(resourceName: "ic_friends_auth"),#imageLiteral(resourceName: "ic_padlock_auth")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCustomLabel()
         setupViewDelegate()
+        self.registerNibs()
+        self.setUpCollectionView()
+        initializePageIndicatorView()
+        
     }
+    
+    func initializePageIndicatorView(){
+        pageIndicatorView = PageIndicatorView(frame: viewPages.frame)
+        pageIndicatorView?.delegate = self
+        viewPages.addSubview(pageIndicatorView!)
+        pageIndicatorView?.centerXAnchor.constraint(equalTo: viewPages.centerXAnchor).isActive = true
+        pageIndicatorView?.centerYAnchor.constraint(equalTo: viewPages.centerYAnchor).isActive = true
+        pageIndicatorView?.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func updateCell(){
+        collectionView.reloadData()
+        pageIndicatorView?.reload()
+    }
+    
+    func setUpCollectionView() {
+        let flowLayout = collectionView.collectionViewLayout as? CenterCellCollectionViewFlowLayout
+        flowLayout?.centerOffset = CGPoint(x: collectionView.frame.width/2, y: 0)
+        
+        let margin: CGFloat = 2.0
+        
+        flowLayout?.sectionInset = UIEdgeInsetsMake(0, margin, 0, margin)
+        flowLayout?.minimumLineSpacing = margin
+
+    }
+    
+    
     
     func setupViewDelegate() {
         presenter.setViewDelegate(delegate: self)
+    }
+    
+    func registerNibs (){
+        collectionView.registerNibFrom(PageCollectionViewCell.self)
     }
     
     func setupCustomLabel(){
@@ -38,7 +78,7 @@ class AuthenticationMainViewController: UIViewController {
         let textWelcomeDescriptionLabel = NSMutableAttributedString(string: defaultTextDescriptionWelcome, attributes: [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont.montserratLight(size: defaultSizeFontWelcomeLabel)])
         
         textWelcomeLabel.append(textWelcomeDescriptionLabel)
-        self.welcomeLabel.attributedText = textWelcomeLabel
+        //self.welcomeLabel.attributedText = textWelcomeLabel
     }
     
     
@@ -111,7 +151,23 @@ class AuthenticationMainViewController: UIViewController {
         }
     }
     
+   
 }
+
+extension AuthenticationMainViewController{
+    
+    
+    func generatePageCell (_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PageCollectionViewCell.identifier, for: indexPath) as! PageCollectionViewCell
+        cell.imagePageView.image = allImages[indexPath.row]
+        cell.titleText.text = allText[indexPath.row]
+        
+        return cell
+    }
+}
+
+
 
 extension AuthenticationMainViewController: AuthenticationDelegate {
     func showHome() {
@@ -131,4 +187,76 @@ extension AuthenticationMainViewController: AuthenticationDelegate {
     }
 }
 
+extension AuthenticationMainViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+ 
+ 
+    }
+    
 
+}
+
+extension AuthenticationMainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            return generatePageCell(collectionView, cellForItemAt:indexPath)
+    }
+
+}
+extension AuthenticationMainViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+        return CGSize(width: PageCollectionViewCell.cellSize.width * UIView.heightScaleProportion(), height: PageCollectionViewCell.cellSize.height * UIView.heightScaleProportion())
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 2
+    }
+  
+}
+
+extension AuthenticationMainViewController: PageIndicatorViewDelegate {
+    
+    var numberOfPages: Int {
+        return 3
+    }
+    
+    var indicatorHeight: CGFloat {
+        return 6.0
+    }
+    
+    var defaultWidth: CGFloat {
+        return 7.0
+    }
+    
+    var selectedWidth: CGFloat {
+        return 20.0
+    }
+    
+    var defaultAlpha: CGFloat {
+        return 0.5
+    }
+    
+    var selectedAlpha: CGFloat {
+        return 1.0
+    }
+    
+    var animationDuration: Double {
+        return 0.2
+    }
+    
+    var indicatorsColor: UIColor {
+        return UIColor.white
+    }
+    
+    var stackViewConfig: (axis: UILayoutConstraintAxis, alignment: UIStackViewAlignment, distribution: UIStackViewDistribution, spacing: CGFloat) {
+        
+        return (.horizontal, alignment: .center, distribution: .equalSpacing, spacing: 5.0)
+    }
+}
