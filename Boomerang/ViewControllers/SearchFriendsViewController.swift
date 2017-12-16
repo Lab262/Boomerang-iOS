82e7d335d8251a2ae89af1c017ee88c8bb3e09f5
Thiago Bernardes <tmb0710@gmail.com>
2017-12-16 00:45:42 -0500

    Busca marot com empty state agor

--- Boomerang/ViewControllers/SearchFriendsViewController.swift
+++ Boomerang/ViewControllers/SearchFriendsViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class SearchFriendsViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchResultEmptyStateLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     let tableViewTopInset: CGFloat = 10.0
     var presenter = SearchFriendsPresenter()
     let tableViewBottomInset = CGFloat(80.0)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         registerNib()
@@ -24,38 +25,38 @@ class SearchFriendsViewController: UIViewController {
         tableView.contentInset = UIEdgeInsetsMake(0, 0, tableViewBottomInset, 0)
         self.tableView.tableFooterView = refreshIndicatorInTableViewFooter()
     }
-
+    
     func getProfiles() {
         presenter.getProfiles(refresh: false)
     }
-
+    
     func setupViewDelegate() {
         presenter.setViewDelegate(view: self)
     }
-
+    
     func registerNib(){
         tableView.registerNibFrom(SearchFriendsTableViewCell.self)
     }
-
+    
     func generateTitleCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
         return cell
     }
-
+    
     func generateSearchFriendCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: SearchFriendsTableViewCell.identifier, for: indexPath) as! SearchFriendsTableViewCell
         cell.presenter.profile = presenter.profiles[indexPath.row-1]
         cell.setupCellInformations(fetchAlreadyFollowing: true)
         return cell
     }
-
+    
     func refreshIndicatorInTableViewFooter() -> UIView {
         let viewIndicator = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 40))
         viewIndicator.backgroundColor = UIColor.clear
         return viewIndicator
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let controller = segue.destination as? ProfileMainViewController {
             controller.presenter.setProfile(profile: presenter.profiles[tableView.indexPathForSelectedRow!.row-1])
@@ -65,7 +66,7 @@ class SearchFriendsViewController: UIViewController {
 
 extension SearchFriendsViewController: UIScrollViewDelegate {
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-
+        
         if (scrollView.contentOffset.y + scrollView.frame.size.height) >= scrollView.contentSize.height {
             if self.presenter.currentSearchString.characters.count > 0 {
                 presenter.searchProfiles(searchString: self.presenter.currentSearchString, refresh: false)
@@ -74,41 +75,47 @@ extension SearchFriendsViewController: UIScrollViewDelegate {
             }
         }
     }
-
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.view.endEditing(true)
     }
 }
 
 extension SearchFriendsViewController : UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         switch indexPath.row {
         case 0:
             return generateTitleCell(tableView, cellForRowAt: indexPath)
         default:
-            return generateSearchFriendCell(tableView, cellForRowAt: indexPath)
+                return generateSearchFriendCell(tableView, cellForRowAt: indexPath)
         }
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
+        if self.presenter.profiles.count > 0 {
+            self.searchResultEmptyStateLabel.isHidden = true
+        } else {
+            self.searchResultEmptyStateLabel.isHidden = false
+        }
+        
         return presenter.profiles.count+1
     }
 }
 
 extension SearchFriendsViewController: UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
         if indexPath.row >= 1 {
             performSegue(withIdentifier: SegueIdentifiers.searchFriendsToProfile, sender: self)
         }
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-
+        
         switch indexPath.row {
         case 0:
             if self.presenter.currentSearchString.characters.count > 0 {
@@ -126,23 +133,23 @@ extension SearchFriendsViewController: SearchFriendsDelegate {
     func reload() {
         tableView.reloadData()
     }
-
+    
     func showMessage(msg: String) {
         // message error
     }
-
+    
     func loadingView() {
         self.view.loadAnimation()
     }
-
+    
     func unloadingView() {
         self.view.unload()
     }
-
+    
     func startFooterLoading() {
         tableView.tableFooterView?.loadAnimation(0.2, UIColor.white, UIActivityIndicatorViewStyle.gray, 1.0)
     }
-
+    
     func finishFooterLoading() {
         self.tableView.tableFooterView?.unload()
     }
@@ -154,7 +161,7 @@ extension SearchFriendsViewController {
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-
+    
     func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -170,7 +177,7 @@ extension SearchFriendsViewController: UISearchBarDelegate {
             presenter.getProfiles(refresh: true)
         }
     }
-
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         self.view.endEditing(true)
     }
